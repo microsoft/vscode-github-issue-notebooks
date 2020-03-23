b66970ee090e70eb19d92c2012996c7f1c749834
Johannes Rieken <johannes.rieken@gmail.com>
2020-03-23 11:13:16 +0100

    add some project tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+import { Project } from '../../project';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('Project', () => {
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	test('asQueryData', () => {
+
+		async function assertQueryData(content: string, expected: string[] = [content]) {
+			const doc = await vscode.workspace.openTextDocument({ language: 'github-issues', content });
+			const project = new Project();
+			project.getOrCreate(doc);
+
+			const data = project.queryData(doc);
+			for (let item of data) {
+				assert.equal(item.q, expected.shift());
+			}
+			assert.equal(expected.length, 0, expected.toString());
+		}
+
+		assertQueryData('foo repo:bar');
+		assertQueryData('$bar=bazz\n$bar repo:bar', ['bazz repo:bar']);
+		assertQueryData('$bar=bazz', []);
+		assertQueryData('foo OR bar', ['foo', 'bar']);
+
 	});
 });
