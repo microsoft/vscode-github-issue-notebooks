2c0a8b6c63c46e990a472784a39138adaff92396
Johannes Rieken <johannes.rieken@gmail.com>
2020-03-24 12:31:58 +0100

    add open/closed icons

--- src/notebookProvider.ts
+++ src/notebookProvider.ts
@@ -203,10 +203,19 @@ export function getHtmlStub(): string {
 	.user img {
 		padding: 0.1em;
 	}
+	.item-state {
+		flex: shrink;
+		padding: 0 .3em;
+		opacity: 60%;
+	}
 </style>`;
 }
 
 export function renderItemAsHtml(item: SearchIssuesAndPullRequestsResponseItemsItem): string {
+
+	const closed = `<svg class="octicon octicon-issue-closed closed" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7 10h2v2H7v-2zm2-6H7v5h2V4zm1.5 1.5l-1 1L12 9l4-4.5-1-1L12 7l-1.5-1.5zM8 13.7A5.71 5.71 0 012.3 8c0-3.14 2.56-5.7 5.7-5.7 1.83 0 3.45.88 4.5 2.2l.92-.92A6.947 6.947 0 008 1C4.14 1 1 4.14 1 8s3.14 7 7 7 7-3.14 7-7l-1.52 1.52c-.66 2.41-2.86 4.19-5.48 4.19v-.01z"></path></svg>`;
+	const open = `<svg class="octicon octicon-issue-opened open" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 011.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>`;
+
 
 	function getContrastColor(color: string): string {
 		// Color algorithm from https://stackoverflow.com/questions/1855884/determine-font-color-based-on-background-color
@@ -218,6 +227,7 @@ export function renderItemAsHtml(item: SearchIssuesAndPullRequestsResponseItemsI
 
 	return `
 <div style="display: flex; padding: .5em 1em;">
+	<div class="item-state">${item.closed_at ? closed : open}</div>
 	<div style="flex: auto;">
 	<a href="${item.html_url}" class="title">${item.title}</a>
 	${item.labels.map(label => `<span class="label" style="background-color: #${label.color};"><a style="color: ${getContrastColor(label.color)};">${label.name}</a></span>`).join('')}
