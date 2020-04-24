2f5aa49ed49fbdf4bc6cf0b65d81d3314fc56f9f
Johannes Rieken <johannes.rieken@gmail.com>
2020-04-24 19:12:28 +0200

    don't accidentally mark cell as runnable

--- src/commands.ts
+++ src/commands.ts
@@ -8,22 +8,24 @@ import * as vscode from 'vscode';
 export function registerCommands(context: vscode.ExtensionContext) {
 	// commands
 	context.subscriptions.push(vscode.commands.registerCommand('github-issues.lockCell', (cell: vscode.NotebookCell) => {
-		cell.metadata = { editable: false, runnable: true };
+		cell.metadata = { ...cell.metadata, editable: false };
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('github-issues.unlockCell', (cell: vscode.NotebookCell) => {
-		cell.metadata = { editable: true, runnable: true };
+		cell.metadata = { ...cell.metadata, editable: true };
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('github-issues.unlockDocument', () => {
-		if (vscode.notebook.activeNotebookDocument) {
-			vscode.notebook.activeNotebookDocument.metadata = { editable: true, cellEditable: true, cellRunnable: true };
+		if (vscode.notebook.activeNotebookEditor) {
+			const { document } = vscode.notebook.activeNotebookEditor;
+			document.metadata = { ...document.metadata, editable: true, cellEditable: true };
 		}
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('github-issues.lockDocument', () => {
-		if (vscode.notebook.activeNotebookDocument) {
-			vscode.notebook.activeNotebookDocument.metadata = { editable: false, cellEditable: false, cellRunnable: true };
+		if (vscode.notebook.activeNotebookEditor) {
+			const { document } = vscode.notebook.activeNotebookEditor;
+			document.metadata = { ...document.metadata, editable: false, cellEditable: false };
 		}
 	}));
 }
