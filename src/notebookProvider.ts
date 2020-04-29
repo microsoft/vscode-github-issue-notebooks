40fcef8ef4d639fe6b630abe16b153937eb2d335
rebornix <penn.lv@gmail.com>
2020-04-28 17:27:52 -0700

    add title for links for better accessibility

--- src/notebookProvider.ts
+++ src/notebookProvider.ts
@@ -16,6 +16,22 @@ interface RawNotebookCell {
 	editable?: boolean;
 }
 
+const entityMap: Record<string, string> = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	"'": '&#39;',
+	'/': '&#x2F;',
+	'`': '&#x60;',
+	'=': '&#x3D;'
+};
+
+function escapeHtml(string: string) {
+	return string.replace(/[&<>"'`=\/]/g, s => entityMap[s]);
+}
+
+
 export class IssuesNotebookProvider implements vscode.NotebookProvider {
 
 	constructor(
@@ -180,9 +196,9 @@ export class IssuesNotebookProvider implements vscode.NotebookProvider {
 			seen.add(item.id);
 
 			// markdown
-			md += `- [#${item.number}](${item.html_url}) ${item.title} [${item.labels.map(label => `${label.name}`).join(', ')}]`;
+			md += `- [#${item.number}](${item.html_url} "${escapeHtml(item.title)}") ${item.title} [${item.labels.map(label => `${label.name}`).join(', ')}]`;
 			if (item.assignee) {
-				md += `- [@${item.assignee.login}](${item.assignee.html_url})\n`;
+				md += `- [@${item.assignee.login}](${item.assignee.html_url} "Issue ${item.number} is assigned to ${escapeHtml(item.assignee.login)}")\n`;
 			}
 			md += '\n';
 
@@ -353,22 +369,6 @@ export function renderItemAsHtml(item: SearchIssuesAndPullRequestsResponseItemsI
 
 	const closed = `<svg class="octicon octicon-issue-closed closed" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7 10h2v2H7v-2zm2-6H7v5h2V4zm1.5 1.5l-1 1L12 9l4-4.5-1-1L12 7l-1.5-1.5zM8 13.7A5.71 5.71 0 012.3 8c0-3.14 2.56-5.7 5.7-5.7 1.83 0 3.45.88 4.5 2.2l.92-.92A6.947 6.947 0 008 1C4.14 1 1 4.14 1 8s3.14 7 7 7 7-3.14 7-7l-1.52 1.52c-.66 2.41-2.86 4.19-5.48 4.19v-.01z"></path></svg>`;
 	const open = `<svg class="octicon octicon-issue-opened open" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 011.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>`;
-
-
-	let entityMap: Record<string, string> = {
-		'&': '&amp;',
-		'<': '&lt;',
-		'>': '&gt;',
-		'"': '&quot;',
-		"'": '&#39;',
-		'/': '&#x2F;',
-		'`': '&#x60;',
-		'=': '&#x3D;'
-	};
-
-	function escapeHtml(string: string) {
-		return string.replace(/[&<>"'`=\/]/g, s => entityMap[s]);
-	}
 
 	function getContrastColor(color: string): string {
 		// Color algorithm from https://stackoverflow.com/questions/1855884/determine-font-color-based-on-background-color
