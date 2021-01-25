1e30839c21c7d4e90948a04d87002dca15122baa
Johannes Rieken <johannes.rieken@gmail.com>
2021-01-25 18:55:34 +0100

    replace webpack with esbuild

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,9 +2,16 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
+
 import { h, render } from 'preact';
 import { AllItems } from './renderer';
-import './style.css';
+import './renderer.css'; // only here for the bundler to pick it up
+
+declare const scriptUrl: string;
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = scriptUrl.split('/').slice(0, -1).concat('renderer.css').join('/');
+document.head.appendChild(link);
 
 const api = acquireNotebookRendererApi('github-issues');
 
