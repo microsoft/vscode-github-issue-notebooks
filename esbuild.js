3f91f9dc8d494b9489fd7897cb57cc6c93bfc88f
Johannes Rieken <johannes.rieken@gmail.com>
2021-01-28 16:04:20 +0000

    use esbuild-script, task-json fiddle

---
+++ esbuild.js
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+const esbuild = require('esbuild');
+
+// extension
+esbuild.build({
+	bundle: true,
+	entryPoints: ['src/extension/extension.ts'],
+	tsconfig: 'src/extension/tsconfig.json',
+	outfile: 'dist/extension.js',
+	external: ['vscode'],
+	format: 'cjs',
+	platform: 'node',
+	sourcemap: true,
+	minify: false // AbortSignal-module has issues...
+}).catch(err => console.err(err)).finally('[done] build extension');
+
+// renderer
+esbuild.build({
+	bundle: true,
+	entryPoints: ['src/renderer/index.tsx'],
+	tsconfig: 'src/renderer/tsconfig.json',
+	outfile: 'dist/renderer.js',
+	format: 'iife',
+	platform: 'browser',
+	sourcemap: true,
+	minify: true,
+}).catch(err => console.err(err)).finally('[done] build renderer');
