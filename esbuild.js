27bf6ea885bdb983567013fa4d6489995c014c09
Johannes Rieken <johannes.rieken@gmail.com>
2021-02-02 11:49:21 +0100

    add bundle-watch script and task

--- esbuild.js
+++ esbuild.js
@@ -3,29 +3,48 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+// @ts-check
+
 const esbuild = require('esbuild');
+const fs = require('fs');
+const path = require('path');
 
-// extension
-esbuild.build({
-	bundle: true,
-	entryPoints: ['src/extension/extension.ts'],
-	tsconfig: 'src/extension/tsconfig.json',
-	outfile: 'dist/extension.js',
-	external: ['vscode'],
-	format: 'cjs',
-	platform: 'node',
-	sourcemap: true,
-	minify: false // AbortSignal-module has issues...
-});
+const incremental = process.argv.includes('--watch');
 
-// renderer
-esbuild.build({
-	bundle: true,
-	entryPoints: ['src/renderer/index.tsx'],
-	tsconfig: 'src/renderer/tsconfig.json',
-	outfile: 'dist/renderer.js',
-	format: 'iife',
-	platform: 'browser',
-	sourcemap: true,
-	minify: true,
-});
+(async function () {
+
+	// extension
+	const buildExtension = await esbuild.build({
+		incremental,
+		bundle: true,
+		entryPoints: ['src/extension/extension.ts'],
+		tsconfig: 'src/extension/tsconfig.json',
+		outfile: 'dist/extension.js',
+		external: ['vscode'],
+		format: 'cjs',
+		platform: 'node',
+		sourcemap: true,
+		minify: false // AbortSignal-module has issues...
+	});
+
+	// renderer
+	const buildRenderer = await esbuild.build({
+		incremental,
+		bundle: true,
+		entryPoints: ['src/renderer/index.tsx'],
+		tsconfig: 'src/renderer/tsconfig.json',
+		outfile: 'dist/renderer.js',
+		format: 'iife',
+		platform: 'browser',
+		sourcemap: true,
+		minify: true,
+	});
+
+
+	if (incremental) {
+		fs.watch(path.join(__dirname, 'src'), { recursive: true }, async function () {
+			await buildExtension.rebuild();
+			await buildRenderer.rebuild();
+		});
+	}
+})();
