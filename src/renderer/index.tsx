855c12437583b58f5bdd3be70093b0e26db63620
Johannes Rieken <johannes.rieken@gmail.com>
2021-05-04 12:33:43 +0200

    return of webpack

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -5,13 +5,7 @@
 
 import { h, render } from 'preact';
 import { AllItems } from './renderer';
-import './renderer.css'; // only here for the bundler to pick it up
-
-declare const scriptUrl: string;
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = scriptUrl.split('/').slice(0, -1).concat('renderer.css').join('/');
-document.head.appendChild(link);
+import './renderer.css';
 
 const api = acquireNotebookRendererApi('github-issues');
 
